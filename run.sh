2239fb1fe0942b3104620b3dc45e4316b618f114
Borja Burgos <borja@tutumcloud.com>
2014-02-13 17:18:57 -0600

    Update run.sh
    fixing issue where statement "Skipped creation of database $WORDPRESS_DB_NAME – it already exists" was always being displayed.

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-if [ -f /.mysql_db_created ]; then
+if [ -f /.mysql_db_created ]; 
+then
         exec supervisord -n
         exit 1
 fi
 
 DB_EXISTS=$(mysql -uadmin -p$DB_PASSWORD -h$DB_PORT_3306_TCP_ADDR -P$DB_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE '"$WORDPRESS_DB_NAME"';" | grep "$WORDPRESS_DB_NAME" > /dev/null; echo "$?")
 
-if [[ DB_EXISTS -eq 1 ]]; then
+if [[ DB_EXISTS -eq 1 ]]; 
+then
         echo "=> Creating database $WORDPRESS_DB_NAME"
         RET=1
         while [[ RET -ne 0 ]]; do
@@ -15,8 +17,9 @@ if [[ DB_EXISTS -eq 1 ]]; then
                 RET=$?
         done
         echo "=> Done!"
+else
+        echo "=> Skipped creation of database $WORDPRESS_DB_NAME – it already exists."
 fi
 
-echo "=> Skipped creation of database $WORDPRESS_DB_NAME – it already exists."
 touch /.mysql_db_created
 exec supervisord -n
